ebc2608b72036b8c7cfe135dc158cc87da521292
jorgenhlehne <32197885+jorgenhlehne@users.noreply.github.com>
2018-03-22 20:44:24 +0100

    La til mulighet for aa registrere apparater i GUI

--- src/backend/GUIController.java
+++ src/backend/GUIController.java
@@ -1,7 +1,31 @@
 package backend;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class GUIController {
 	
+	@FXML TextField apparatusNameInput;
+	@FXML TextArea apparatusDescInput;
+	@FXML Label apparatusRegisterLabel;
+	/*
+	@FXML TextField 
+	@FXML TextField*/
+	
+	Register register;
+	
+	public void registerApparatus() {
+		String apName = apparatusNameInput.getText();
+		String apDesc = apparatusDescInput.getText();
+		if ( apName == "" || apDesc == "") {
+			apparatusRegisterLabel.setText("Invalid input");
+		} else {
+			Register.RegisterAparatus(apName, apDesc);
+		}
+	}
+
 	//TODO: Implementere metoder for å kontrollere GUIen
 
 }
