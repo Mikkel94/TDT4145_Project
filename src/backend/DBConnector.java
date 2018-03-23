88a651bcd150ac93b6978c66f16d765f958c039e
Mikkel Sandsbråten <mikkel.sandsbraaten@gmail.com>
2018-03-23 23:15:44 +0100

    ntnu mysql server added

--- src/backend/DBConnector.java
+++ src/backend/DBConnector.java
@@ -15,8 +15,12 @@ public class DBConnector {
 
     public static Connection connect() {
         try {
-            String url = "jdbc:mysql://localhost:3306/WorkOutDiary?useSSL=false"; // For dev on localhost
-            conn = DriverManager.getConnection(url, "root", "car22");
+            //String url = "jdbc:mysql://localhost:3306/WorkOutDiary?useSSL=false"; // For dev on localhost
+            //conn = DriverManager.getConnection(url, "root", "car22");
+            
+            String url = "jdbc:mysql://mysql.stud.ntnu.no:3306/mikkesa_tdt4145?useSSL=false";
+            conn = DriverManager.getConnection(url, "mikkesa_db", "rullekake");
+    			
 
 
         } catch (Exception e) {
