bab349bfe9facbd6a8cca9372c608698701f7b16
fstorehaug <fstorehaug@hotmail.com>
2018-03-23 14:37:57 +0100

    Lagt til querrytxt, generell funksjon for å gjøre rs til String

--- src/frontend/GUIController.java
+++ src/frontend/GUIController.java
@@ -5,6 +5,8 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
+import java.sql.ResultSet;
+
 import backend.Register;
 
 public class GUIController {
@@ -26,6 +28,31 @@ public class GUIController {
 		} else {
 			Register.RegisterAparatus(apName, apDesc);
 		}
+	}
+
+	public void getNWorkouts()
+	{
+		int n = nInput.getInt();
+		int userID = UserIdInput.getInt();
+		ResultSet rs = backend.retiveWorkout(n, userID);
+		Sting output = querrytxt(rs);
+		viewNLastLable.setText(output);
+	}
+
+	private String querrytxt(ResultSet rs)
+	{
+		String output = "";
+		ResultSetMetaData rsmeta = rs.getMetaData();
+		int n = rsmeta.getColumnCount();
+		while (rs.next())
+		{
+			for(int i = 1; i <=n; i++)
+			{
+				output += rsmd.getColumnName(i) + ": " + fs.getsting(i) + ", "; ;
+			}
+			output += "/n";
+		}
+		return output;
 	}
 
 	//TODO: Implementere metoder for � kontrollere GUIen
